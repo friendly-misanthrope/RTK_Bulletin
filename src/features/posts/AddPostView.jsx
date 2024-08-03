96ca3373c9a3e3ff81eec154485c250db08f5d65
friendly-misanthrope <nickthedevguy@gmail.com>
2024-08-03 01:29:33 -0700

    created addPost form and input for post title

--- src/features/posts/AddPostView.jsx
+++ src/features/posts/AddPostView.jsx
@@ -6,10 +6,25 @@ const AddPostView = () => {
     body: ''
   }
 
+  const {title, body} = post;
+
   const postChangeHandler = (e) => {
     setPost(prevState => {return {...prevState, [e.target.name]: e.target.value}})
   }
 
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="title">Post Title: </label>
+        <input type="text"
+        id="title"
+        name="title"
+        value={title}
+        onChange={postChangeHandler} />
+      </form>
+    </section>
+  )
 }
 
 export default AddPostView;
