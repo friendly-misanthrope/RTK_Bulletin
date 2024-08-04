dd3940a93829ea7223f7de4fb4e599e8872408b4
friendly-misanthrope <nickthedevguy@gmail.com>
2024-08-04 02:46:12 -0700

    Created side effect for postsStatus and dispatch()

---
+++ src/features/posts/PostsExcerpt.jsx
@@ -0,0 +1,18 @@
+import PostAuthorView from "./PostAuthorView";
+import CreatedAt from "./CreatedAt";
+import ReactionsView from "./ReactionsView";
+
+const PostsExcerpt = ({ post }) => {
+  return (
+    <article>
+      <h3>{post.title}</h3>
+      <p>{post.body.substring(0, 100)}</p>
+      <p className="postCredit">
+        <PostAuthorView userId={post.userId} />
+        <CreatedAt timestamp={post.createdAt} />
+      </p>
+      <ReactionsView post={post} />
+    </article>
+  )
+}
+export default PostsExcerpt;
