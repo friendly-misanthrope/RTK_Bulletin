18552a33a66ad6f9f59eaf739c4060bfc40f6f85
friendly-misanthrope <nickthedevguy@gmail.com>
2024-08-04 00:33:13 -0700

    added extra reducer cases for fetchPosts pending, fulfilled, and rejected

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,16 +1,17 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { nanoid } from "@reduxjs/toolkit";
+import { sub } from 'date-fns';
 import axios from 'axios';
 const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts'
 
 const initialState = {
   posts: [],
-  status: 'idle', // 'idle' || 'loading' || 'succeeded' || 'failed'
+  status: 'idle', // 'idle' || 'pending' || 'fulfilled' || 'rejected'
   error: null
 };
 
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', () => {
-  const response = axios.get(POSTS_URL)
+  axios.get(POSTS_URL)
     .then((res) => [...res.data]);
 });
 
@@ -48,7 +49,33 @@ const postsSlice = createSlice({
         post.reactions[reaction]++
       }
     }
-  }
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = 'pending';
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'fulfilled'
+        let min = 1;
+        const posts = action.payload.map(post => {
+          post.createdAt = sub(new Date(), { minutes: min++ }).toISOString();
+          post.reactions = {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+          }
+          return post
+        });
+        state.posts = state.posts.concat(posts);
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'rejected';
+        state.error = action.error.message
+      });
+  } 
 });
 
 export const selectAllPosts = (state) => state.posts.posts;
