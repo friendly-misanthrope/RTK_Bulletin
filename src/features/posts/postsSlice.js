ee99ffb52413758f777bb0b2b8f6820b22e2f259
friendly-misanthrope <nickthedevguy@gmail.com>
2024-08-03 23:36:45 -0700

    removed hard-coded initialState. Beginning of async/thunk actions

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -2,34 +2,11 @@ import { createSlice } from "@reduxjs/toolkit";
 import { nanoid } from "@reduxjs/toolkit";
 import { sub } from 'date-fns';
 
-const initialState = [
-  {
-    id: '1',
-    title: 'Learning Redux Toolkit',
-    body: "I've heard good things",
-    createdAt: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0
-    }
-  },
-  {
-    id: '2',
-    title: 'Slices...',
-    body: "The more I say slice, the more I want pizza.",
-    createdAt: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: {
-      thumbsUp: Number(0),
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0
-    }
-  }
-];
+const initialState = {
+  posts: [],
+  status: 'idle',
+  error: null
+};
 
 const postsSlice = createSlice({
   name: 'posts',
